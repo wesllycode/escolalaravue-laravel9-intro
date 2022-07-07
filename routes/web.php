b9ecd284466e8166668f958b0cc837cffef3cc95
test <test@gmnai.om>
2022-07-07 03:43:24 -0300

    fix: grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,26 @@ Route::get('/users/{naoprecisanomeserigual}', function ($id) {
 });
 
 
-
 Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) {
     return $paramA . ' - '. $paramB;
 });
 
 Route::get('/empresa/{string?}', function ($string = null) {
     return $string;
+});
+
+// Criando um grupo de rotas - 05:20
+Route::prefix('usuarios')->group(function() {
+
+    Route::get('/', function() {
+        return 'Listar Todos os Usuários';
+    });
+
+    Route::get('/{id}', function ($id) {
+        return 'Visualizar os detalhe do usuário do ID->'.$id;
+    });
+
+    Route::get('/edit/{id}', function ($id){
+        return 'Editar Usuário do ID -> '.$id;
+    });
 });
