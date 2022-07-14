c853e1a91516ac719588fed9a0509e1120aac101
wesllycode <wesllycode07@gmail.com>
2022-07-14 20:31:10 -0300

    fix: troquei a url  da rota

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix('mp')->group(function() {
-    Route::get('/payment',[PaymentController::class,'pagamento']);
+    Route::get('/payment',[PaymentController::class,'index']);
 });
