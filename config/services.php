4dfc8f98839434e4bb685ca8232ec2c00157c9b5
wesllycode <wesllycode07@gmail.com>
2022-07-15 02:53:25 -0300

    fix: criando e setando as variaves de ambiente do laravel para o mercadopago

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'mercadopago' => [
+        'key' => env('MERCADOPAGO_KEY'),
+        'secret' => env('MERCADOPAGO_SECRET'),
+    ],
+
 ];
